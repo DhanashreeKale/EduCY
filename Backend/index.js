66a1268c630f9d8921979568ecfd82df9cd89bac
Purva-R <87204590+Purva-R@users.noreply.github.com>
2021-08-29 22:08:45 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -8,7 +8,7 @@ const app = express();
 
 mongoose.connect('mongodb://localhost/educy')
 .then(() => console.log('Connected to MongoDB...'))
-.catch(err => console.error('Could not connect to MongoBD'));
+.catch(err => console.error('Could not connect to MongoDB...'));
 
 app.use(express.json());
 app.use('/api/students',students);
