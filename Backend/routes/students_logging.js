121e8692b267d019bb9c1d70b1093ba7e586a63a
DhanashreeKale <80916999+DhanashreeKale@users.noreply.github.com>
2021-09-03 14:41:09 +0530

    login of student

--- Backend/routes/students_logging.js
+++ Backend/routes/students_logging.js
@@ -1,60 +1,63 @@
-const {studentLogin, validateStudentLogin} = require('../models/student_login');
+const { studentLogin, validateStudentLogin } = require('../models/student_login');
 const express = require('express');
 const router = express.Router();
 
-router.get('/logs', async(req,res)=>{
-    const studLog= await studentLogin.find();
-    res.send(studLog);    
+router.get('/logs', async (req, res) => {
+    const studLog = await studentLogin.find();
+    res.send(studLog);
 });
 
 
-router.post('/', async(req, res) => {
-    const {error}=validateStudentLogin(req.body);
-    if(error)
-    return res.status(400).send(error.details[0].message);
+router.post('/receive', async (req, res) => {
+    const { error } = validateStudentLogin(req.body);
+    if (error)
+        return res.status(400).send(error.details[0].message);
 
-    let studLog= new studentLogin({
-        student_username:req.body.student_username,
-        student_password:req.body.student_password
+    let studLog = new studentLogin({
+        student_username: req.body.student_username,
+        student_password: req.body.student_password
     });
 
-    studLog= await studLog.save();
-    res.send(studLog);
+    const check = await studentLogin.collection.findOne({ student_username: studLog.student_username });
+    if (!check)
+        res.json({ status: "false", msg: "User doesn't exist." });
+    else
+        res.json({ status: "true", msg: "User login succesfull" });
 });
 
-router.put('/:id', async(req,res)=>{
-    const {error} = validateStudentLogin(req.body);
-    if(error)
-    return res.status(400).send(error.details[0].message);
+router.put('/:id', async (req, res) => {
+    const { error } = validateStudentLogin(req.body);
+    if (error)
+        return res.status(400).send(error.details[0].message);
 
-    const studLog= await studentLogin.findByIdAndUpdate(req.params.id,
+    const studLog = await studentLogin.findByIdAndUpdate(req.params.id,
         {
-            student_username:req.body.student_username,
-            student_password:req.body.student_password
-        },{new: true});
+            student_username: req.body.student_username,
+            student_password: req.body.student_password
+        }, { new: true });
 
-    if(!studLog)
-    return res.status(404).send('The student with the login info not found.');
+    if (!studLog)
+        return res.status(404).send('The student with the login info not found.');
 
     res.send(studLog);
 });
 
-router.delete('/:id', async(req, res)=>{
+router.delete('/:id', async (req, res) => {
     const studLog = await studentLogin.findByIdAndRemove(req.params.id);
 
-    if(!studLog)
-    return res.status(404).send('The student with the login info not found.');
+    if (!studLog)
+        return res.status(404).send('The student with the login info not found.');
 
     res.send(studLog);
 });
 
 router.get('/:id', async (req, res) => {
     const studLog = await studentLogin.findById(req.params.id);
-  
-    if (!studLog) 
-    return res.status(404).send('The student with the login info not found..');
-  
+
+    if (!studLog)
+        return res.status(404).send('The student with the login info not found..');
+
     res.send(studLog);
 });
-    
+
 module.exports = router;
