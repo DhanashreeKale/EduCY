88f79bb6434ecd092915a2c26d32d458ca7a4ba2
DhanashreeKale <80916999+DhanashreeKale@users.noreply.github.com>
2021-09-05 14:52:41 +0530

    Update teacher.js

--- Backend/models/teacher.js
+++ Backend/models/teacher.js
@@ -1,31 +1,45 @@
 const Joi = require('joi');
 const mongoose = require('mongoose');
 
-const Teacher= mongoose.model('Teacher', new mongoose.Schema({
-    name: {
+const Teacher = mongoose.model('Teacher', new mongoose.Schema({
+    firstname: {
         type: String,
-        reuqired: true,
-        minlength: 5,
+        required: true,
+        minlength: 3,
+        maxlength: 50
+    },
+    lastname: {
+        type: String,
+        required: true,
+        minlength: 3,
         maxlength: 50
     },
     email: {
         type: String,
         required: true,
-        minlength: 5,
+        minlength: 10,
         maxlength: 255,
         unique: true
-    
+    },
+    phoneNo: {
+        type: String,
+        required: true,
+        minlength: 10,
+        maxlength: 10
     }
 }));
 
-function validateTeacher(teacher){
-    const schema={
-        name: Joi.string().min(5).max(50).required(),
-        email: Joi.string().min(5).max(255).required()
+function validateTeacher(teacher) {
+    const schema = {
+        firstname: Joi.string().min(3).max(50).required(),
+        lastname: Joi.string().min(3).max(50).required(),
+        email: Joi.string().min(10).max(255).required(),
+        phoneNo: Joi.string().min(10).max(10).required()
     };
 
-    return Joi.validate(teacher,schema);
+    return Joi.validate(teacher, schema);
 }
+
 
 exports.Teacher = Teacher;
 exports.validate = validateTeacher;
