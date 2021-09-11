86b3b3632ef289fca8b1049a0967f63a97923578
DhanashreeKale <80916999+DhanashreeKale@users.noreply.github.com>
2021-09-11 13:45:42 +0530

    Update add_course.js

--- Backend/models/add_course.js
+++ Backend/models/add_course.js
@@ -13,23 +13,13 @@ const courseSchema = new mongoose.Schema({
             required: true
         },
         semester: [{
-            sem1: { type: String },
-            sem2: { type: String },
-            sem3: { type: String },
-            sem4: { type: String }
+            sem1: { type: [String] },
+            sem2: { type: [String] },
+            sem3: { type: [String] },
+            sem4: { type: [String] }
         }]//semester ends here
     }]//batchDetails ends here
 });
-/*
-batchDetails:
-        year:2021
-        semester:
-            sem1:['SC 101','SC 102','SC 103','SC 104']
-            sem2:['SC 201','SC 202']
-            sem3:['SC 301','SC 302','SC 303']
-            sem4:['SC 401','SC 402']
-
-*/
 
 const Course = mongoose.model('Course', courseSchema);
 
